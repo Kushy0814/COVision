398a9fe1596449410a5349aa0f005e2abfd50785
timmy625 <30708141+timmy625@users.noreply.github.com>
2022-02-23 00:29:09 -0500

    Add files via upload

--- lung_diseases_ct_custom_cnn.py
+++ lung_diseases_ct_custom_cnn.py
@@ -75,6 +75,7 @@ print("Images Processed")
 model = models.Sequential()
 model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(128, 128, 1)))
 model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Dropout(0.3))
 model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(128, 128, 1)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Dropout(0.4))
@@ -84,7 +85,7 @@ model.add(layers.Flatten())
 model.add(layers.Dense(256, activation='relu'))
 model.add(layers.Dropout(0.5))
 model.add(layers.Dense(32, activation='relu'))
-model.add(layers.Dropout(0.4))
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(3, activation='softmax'))
 print("Model Created")
 
@@ -160,8 +161,8 @@ metricFile.write("Confusion Matrix: " + matrix_string)
 metricFile.close()
 print("Metrics Calculated")
 
-from tensorflow.keras.utils import plot_model
-plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=False, show_layer_activations=True)
+# from tensorflow.keras.utils import plot_model
+# plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=False, show_layer_activations=True)
 
 model.save('/ct_model')
 print("Model Saved")
